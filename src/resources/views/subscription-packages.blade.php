0c3adb2038376e0b330b9fef25b277af52dcb0a9
Jonathan Rabie <jonathan.rabie@toss-group.com>
2025-02-04 15:06:36 +0100

    Fixed the text wraping for the long description

--- src/resources/views/subscription-packages.blade.php
+++ src/resources/views/subscription-packages.blade.php
@@ -14,8 +14,8 @@
 
     <div class="mt-6 flex gap-6">
         <!-- Feature List -->
-        <div class="flex-1 bg-gray-50 p-6 rounded-lg shadow-sm" id="package-description">
-            test
+        <div class="flex-1 bg-gray-50 p-6 rounded-lg shadow-sm" id="package-description" style="word-wrap: break-word; overflow-wrap: break-word; white-space: normal;">
+            Select a package to see the features
         </div>
 
         <!-- Pricing Options -->
